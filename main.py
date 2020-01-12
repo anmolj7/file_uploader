edaf948aca91a1d02b69dbad0ae2fe5bfae9da06
EmperorAj <codingwithaj@gmail.com>
2020-01-12 14:09:29 +0530

    New Version

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
+import datetime
+import time
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'txt', 'md'}  # Server code to make sure it doesn't happen.
@@ -14,19 +16,31 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def store_file_details(fName, title, upload_time):
+    # Using a file to store the file details because well,
+    # Using a database makes my website vulnerable to the SQL injection attacks
+    # Also, this is a simple app, so, I thought, this'd be better!..
+    with open('files.txt', 'a+') as f:
+        f.write(f'["{upload_time}", "{fName}", "{title}"]' + '\n')
+
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['fileName']
         if file and allowed_file(file.filename):
+            file_title = request.form['file_title']
+            upload_time = str(time.time())
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            store_file_details(filename, file_title, upload_time)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], upload_time.replace(" ", "")+'-'+filename))
             return redirect(url_for('index'))
     return render_template('upload.html')
 
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
+    filename = filename.replace(' ', '')
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
@@ -38,7 +52,9 @@ def static_file(filename):
 
 @app.route('/')
 def index():
-    files = os.listdir(os.path.join(os.getcwd(), 'uploads'))
+    with open('files.txt') as f:
+        files = f.readlines()
+    files = list(map(eval, files))
     return render_template('index.html', files=files)
 
 
